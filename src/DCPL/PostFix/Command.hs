eb79716105cf837038a73f92c71092c79c122263
Jens Thomas <jetho@gmx.de>
2012-09-02 19:09:43 +0200

    add postfix

---
+++ src/DCPL/PostFix/Command.hs
@@ -0,0 +1,12 @@
+
+module DCPL.PostFix.Command
+
+where
+
+
+data Command = Num Int
+             | Eq | Gt | Lt   
+             | Swap | Pop | Sel | NGet | Exec
+             | Add | Sub | Mul | Div | Rem
+             | Seq [Command]
+  deriving (Show, Eq)
